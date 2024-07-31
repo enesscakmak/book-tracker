ee476f606b14ff277bdcee58f48516d77b01df34
Enes Çakmak <cakmakkeness@gmail.com>
2024-07-31 22:46:12 +0300

    feat: add bottom navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.orangeAccent),
         useMaterial3: true,
       ),
       home: const MyHomePage(),
@@ -31,6 +31,19 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text('A. Reader'),
+      ),
+      body: const Column(
+        children: [Text("Hello")],
+      ),
+      bottomNavigationBar: BottomNavigationBar(items: <BottomNavigationBarItem>[
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+        BottomNavigationBarItem(icon: Icon(Icons.save), label: "Saved"),
+        BottomNavigationBarItem(icon: Icon(Icons.favorite), label: "Favorite")
+      ]),
+    );
   }
 }
